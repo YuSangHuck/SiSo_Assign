7a24cc542843d1fa122184c46c3bcfd7b36c9e42
yusanghuck <g01099387636@gmail.com>
2017-12-05 11:21:09 +0900

    1st commit, start

---
+++ HW1/HW1/Scheduler.c
@@ -0,0 +1,13 @@
+#include "Thread.h"
+#include "Init.h"
+#include "Scheduler.h"
+#include "Queue.h"
+
+int	RunScheduler(void){
+
+}
+
+void __ContextSwitch(Thread pCurThread, Thread* pNewThread){
+
+}
+
---
+++ HW1/HW1/main.c
@@ -0,0 +1,11 @@
+#include "Thread.h"
+#include "Init.h"
+#include "Scheduler.h"
+#include "Queue.h"
+
+#include<stdio.h>
+
+int main(void){
+
+}
+
